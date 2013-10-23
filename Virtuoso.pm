a2f8705f1e090cabaca71787476a633dc850d830
Brian Duggan <bduggan@usgcrp.gov>
2013-10-23 13:38:15 -0600

    isql, log

--- Virtuoso.pm
+++ Virtuoso.pm
@@ -1,5 +1,6 @@
 package Virtuoso;
 use Mojo::Base qw/-base/;
+use Mojo::Log;
 use Path::Class qw/file/;
 use IPC::Run qw/run timeout/;
 use Cwd qw/abs_path/;
@@ -10,32 +11,39 @@ has 'user' => sub { die "no user" };
 has 'pass' => sub { die "no pass" };
 has 'port' => 1111;
 has 'graph_uri';
+has 'logger' => sub { state $log ||= Mojo::Log->new->path("/tmp/import-gcis.$$.log") };
 
 sub load_ttl_file {
-    my $s = shift;
-    my $file = shift;
-    my $in;
-    my ($out, $errs);
+    my $s         = shift;
+    my $file      = shift;
     my $graph_uri = $s->graph_uri;
-    #my @cmd = ('vload','ttl', $file, $graph_uri  );
-    #run \@cmd, \$in, \$out, \$errs, timeout(30) or die $!;
-    my $full = abs_path("$file");
-    my $isql = "DB.DBA.TTLP_MT(file_to_string_output('$full'),'','$graph_uri', 255);";
-    $s->_do_isql($isql);
+    my $full      = abs_path("$file");
+    $s->_do_isql( "DB.DBA.TTLP_MT(file_to_string_output('$full'),'','$graph_uri', 255);" );
 }
 
 sub drop_graph {
     my $s = shift;
-    $s->_do_isql("SPARQL CLEAR GRAPH <".$s->graph_uri.">");
+    my $graph_uri = shift || $s->graph_uri;
+    $s->_do_isql("SPARQL CLEAR GRAPH <".$graph_uri.">");
+}
+
+sub rename_graph_to {
+    my $s = shift;
+    my $name = shift;
+    my $old = $s->graph_uri;
+	$s->_do_isql("update DB.DBA.RDF_QUAD TABLE OPTION (index RDF_QUAD_GS) set g = iri_to_id ('$name') where g = iri_to_id ('$old', 0);");
+    $s->graph_uri($name);
+    return $s;
 }
 
 sub _do_isql {
     my $s = shift;
     my $sql = shift or die "missing sql";
     $sql .= ';' unless $sql =~ /;$/;
-    my @cmd = ($s->isql,$s->port,$s->user,$s->pass);
-    my ($out, $err);
-    run \@cmd, \$sql, \$out, $err, timeout(10) or die "@cmd : $!";
+    my @cmd = ( $s->isql, $s->port, $s->user, $s->pass );
+    my ( $out, $err );
+    run \@cmd, \$sql, \$out, \$err, timeout(10) or die "@cmd : $!";
+    $s->logger->debug("$sql : $out : $err");
     return 1;
 }
 
